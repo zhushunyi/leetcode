60f69eebaea9e60d41a42768fb3dee8ad56ccb02
zhushunyi <1308587456@qq.com>
2020-01-09 16:37:37 +0800

    404

---
+++ 404.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if root.left and not root.left.right and not root.left.left:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+            
+
