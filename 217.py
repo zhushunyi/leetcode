fc07bb458e1286f3426f464aa0e68cee7c225d87
zhushunyi <1308587456@qq.com>
2020-01-19 14:01:46 +0800

    217

---
+++ 217.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
+        nums = sorted(nums)
+        for i in range(0,len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
