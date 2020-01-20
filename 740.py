e69149613eaa4b504e26ffe834549d6b0618dc5c
zhushunyi <1308587456@qq.com>
2020-01-20 11:25:19 +0800

    740

---
+++ 740.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution(object):
+    def deleteAndEarn(self, nums):
+        #dynamic programming
+        count = collections.Counter(nums)
+        prev = None
+        avoid = using = 0
+        for k in sorted(count):
+            if k - 1 != prev:
+                avoid, using = max(avoid, using), k * count[k] + max(avoid, using)
+            else:
+                avoid, using = max(avoid, using), k * count[k] + avoid
+            prev = k
+        return max(avoid, using)
